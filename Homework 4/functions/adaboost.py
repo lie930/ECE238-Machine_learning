dc42723cf22e13613b9efb0436d2d95782821fc4
Morten Lie <morli@stud.ntnu.no>
2018-05-17 17:46:05 -0700

    Made the adaboost shell, needs debugging ++

--- Homework 4/functions/adaboost.py
+++ Homework 4/functions/adaboost.py
@@ -1,2 +1,50 @@
-def adaboost():
-    print('Adabost to be made')
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+
+# Algorithm implementation based on notation from paper "Multi-class Adaboost (Zhu et al)"
+
+def adaboost(X_train, Y_train, X_test, Y_test, M, clf_tree):
+    n_train = len(X_train)
+    n_test = len(X_test)
+
+    #Initiate weights
+    w = np.ones(n_train)/n_train
+
+    T = np.zeros((M,n_train))
+    I = np.zeros((M,n_train))
+    err = np.zeros(M)
+    a = np.zeros(M)
+
+    for m in range(M):
+        # Fit a classifier T to the training data using weights w
+        clf_tree.fit(X_train, Y_train, sample_weight = w)
+        T[:][m] = clf_tree.predict(X_train)
+
+        # Compute the error
+        I[m] = [int(x) for x in (T[m] != Y_train)]
+        err[m] = np.sum(np.dot(w,I[m])) / sum(w)
+
+        # Compute alpha
+        a[m] = np.log((1-err[m])/float(err[m]))
+
+        # Update weights
+        for i in range(n_train):
+            w[i] = w[i] * np.exp(a[m]*I[m][i])
+
+    #Return classifier C
+    sum1 = np.zeros((n_train,2))
+    k = [-1,1]
+    for i in range(2):
+        ki = k[i]
+        for m in range(M):
+            print(a[m])
+            print([float(x) for x in (T[m] == ki)])
+            #sum1[:][i] += a[m]*[int(x) for x in (T[m] == ki)]
+'''
+    for i in range(n_train):
+        if sum1[i][1] > sum1[i][2]:
+            C[i] = -1
+        else:
+            C[i] = 1
+    return C
+'''
--- Homework 4/hw4_problem2.py
+++ Homework 4/hw4_problem2.py
@@ -2,13 +2,18 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+from sklearn.tree import DecisionTreeClassifier
 from functions.utils import generate_class_0, generate_class_1
 from functions.adaboost import adaboost
 
 if __name__ == '__main__':
-    N = 100;
+    N = 10;
     # Generate data
     x0 = generate_class_0(math.floor(N/2))
     x1 = generate_class_1(math.floor(N/2))
-    x_train = np.concatenate([x0, x1])
-    y = np.append(-1*np.ones(int(N/2)), np.ones(int(N/2)))
+    X_train = np.concatenate([x0, x1])
+    Y_train = np.append(-1*np.ones(int(N/2)), np.ones(int(N/2)))
+
+    clf_tree = DecisionTreeClassifier(max_depth = 1, random_state = 1)
+    M = 1
+    C = adaboost(X_train, Y_train, X_train, Y_train, M, clf_tree)
